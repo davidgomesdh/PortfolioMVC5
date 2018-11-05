81d9cfb155c5845f607fb1aee35ffbef79dc6ae0
davidgomesdh <davidgomesdosantos@hotmail.com>
2018-11-05 13:23:01 -0200

    Criação da solution, separação das camadas e primeiras inclusões de conteudo nas camadas Presentation e Domain

---
+++ src/PortfolioMVC5.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PortfolioMVC5.UI.MVC.Startup))]
+namespace PortfolioMVC5.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
